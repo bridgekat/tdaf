f4c928c36ddd13cc5a14ff5b43e7f07eac1e6149
Zhanrong Qiao <qiaozhanrong@gmail.com>
2026-08-25 19:16:30 +0800

    surface: Rockafellar §7, closures of convex functions

--- Tdaf.lean
+++ Tdaf.lean
@@ -119,3 +119,4 @@ import Tdaf.Surface.Rockafellar.Part1.Section02
 import Tdaf.Surface.Rockafellar.Part1.Section03
 import Tdaf.Surface.Rockafellar.Part1.Section04
 import Tdaf.Surface.Rockafellar.Part1.Section05
+import Tdaf.Surface.Rockafellar.Part2.Section07
